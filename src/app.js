e6d84626b6824b47d4874787ff35f488bdeb4961
Joe Pham <joe.pham@patroids.com>
2023-03-28 12:27:55 +0700

    U

--- src/app.js
+++ src/app.js
@@ -6,21 +6,22 @@ const morgan = require('morgan');
 const app = express();
 
 // init Middleware
-
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({
+    extends: true
+}));
 
 // init DB
 require('./dbs/init.mongodb');
 const { countConnect, checkOverLoad } = require('./helper/check.connect');
 // checkOverLoad();
+
+
 // init router
-app.get('/',(req, res, next) => {
-    return res.status(200).json({
-        message: 'Welcome NodeJS',
-    });
-});
+app.use('', require('./routers'))
 
 // handle error
 
